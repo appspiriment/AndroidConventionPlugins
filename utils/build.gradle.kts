173431e10a2f28a0e2b7953b34ee134c8e54942d
Arun Shankar <appspiriment@gmail.com>
2025-01-19 23:37:52 +0000

    Publishing Utils to Maven Central

--- utils/build.gradle.kts
+++ utils/build.gradle.kts
@@ -1,6 +1,9 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.google.android.library)
     alias(libs.plugins.kotlin.android)
+    id("com.vanniktech.maven.publish") version "0.28.0"
     `maven-publish`
     signing
 }
@@ -43,11 +46,12 @@ android {
     kotlinOptions {
         jvmTarget = libs.versions.javaVersion.get()
     }
-    publishing {
-        singleVariant("release") {
-            withSourcesJar()
-        }
-    }
+//    publishing {
+//        singleVariant("release") {
+//            withSourcesJar()
+//            withJavadocJar()
+//        }
+//    }
 }
 
 dependencies {
@@ -55,40 +59,104 @@ dependencies {
     testImplementation(libs.junit.test)
 }
 
+//publishing {
+//
+//    publications {
+//        create<MavenPublication>("mavenUtils") {
+//            groupId = "io.github.appspiriment"
+//            artifactId = "utils"
+//            version = libs.versions.appspirimentUtils.get()
+//            pom {
+//                name = "Appspiriment Utils"
+//                description = "A library with common util functions and extension methods to help kotlin developers easily use them."
+//                url = "https://github.com/appspiriment/AndroidConventionPlugins/tree/main/utils"
+//                licenses {
+//                    license {
+//                        name = "The Apache License, Version 2.0"
+//                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
+//                    }
+//                }
+//                developers {
+//                    developer {
+//                        id = "appspiriment"
+//                        name = "Appspiriment Labs"
+//                        email = "appspiriment@gmail.com"
+//                    }
+//                }
+//                scm {
+//                    connection = "scm:git:git://example.com/my-library.git"
+//                    developerConnection = "scm:git:ssh://example.com/my-library.git"
+//                    url = "http://example.com/my-library/"
+//                }
+//            }
+//
+//
+//            afterEvaluate { from(components["release"])}
+//        }
+//    }
+//
+//
+//    // Here we define some repositories that we can publish our outputs to.
+//    repositories {
+//        // Specifying that this is a custom maven repository.
+//        maven {
+//            // This is the name of the repo that is used as the value of ${target}
+//            // from above.
+//            name = "OSSRH"
+//
+//            // Self-explanatory.
+//            url = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2")
+//
+//            // These need to be defined in ~/.gradle/gradle.properties:
+//            // ossrhUsername=<your sonatype jira username>
+//            // ossrhPassword=<your sonatype jira password>
+//            credentials {
+//                username = project.findProperty("ossrhUsername") as String?
+//                password = (project.findProperty("ossrhPassword") as String?).also{
+//                    println("PasswordToken - $it")
+//                }
+//            }
+//            authentication {
+//                create<BasicAuthentication>("basic")
+//            }
+//        }
+//    }
+//}
 
-publishing {
+mavenPublishing {
+    coordinates(
+        groupId = "io.github.appspiriment",
+        artifactId = "utils",
+        version = libs.versions.appspirimentUtils.get()
+    )
 
-    publications {
-        create<MavenPublication>("mavenUtils") {
-            groupId = "io.github.appspirimentlabs"
-            artifactId = "utils"
-            version = libs.versions.appspirimentUtils.get()
-            pom {
-                name = "Appspiriment Utils"
-                description = "A library with common util functions and extension methods to help kotlin developers easily use them."
-                url = "https://github.com/appspirimentlabs/AndroidConventionPlugins/tree/main/utils"
-                licenses {
-                    license {
-                        name = "The Apache License, Version 2.0"
-                        url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
-                    }
-                }
-                developers {
-                    developer {
-                        id = "appspirimentlabs"
-                        name = "Appspiriment Labs"
-                        email = "appspiriment@gmail.com"
-                    }
-                }
-                scm {
-                    connection = "scm:git:git://example.com/my-library.git"
-                    developerConnection = "scm:git:ssh://example.com/my-library.git"
-                    url = "http://example.com/my-library/"
-                }
+    pom {
+        name = "Appspiriment Utils"
+        description = "A library with common util functions and extension methods to help kotlin developers easily use them."
+        url = "https://github.com/appspiriment/AndroidConventionPlugins/tree/main/utils"
+        licenses {
+            license {
+                name = "The Apache License, Version 2.0"
+                url = "http://www.apache.org/licenses/LICENSE-2.0.txt"
             }
-
-
-            afterEvaluate { from(components["release"])}
+        }
+        developers {
+            developer {
+                id = "appspiriment"
+                name = "Appspiriment Labs"
+                email = "appspiriment@gmail.com"
+            }
+        }
+        scm {
+            connection = "scm:git:git://example.com/my-library.git"
+            developerConnection = "scm:git:ssh://example.com/my-library.git"
+            url = "http://example.com/my-library/"
         }
     }
+
+    // Configure publishing to Maven Central
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+
+    // Enable GPG signing for all publications
+    signAllPublications()
 }
